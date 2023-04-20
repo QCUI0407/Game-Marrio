cc1686ee11d7ac4d5e0a3b5dd24b3cdd210ff61c
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2023-04-19 21:58:27 -0500

    add window cannot open

--- src/main/java/jade/Window.java
+++ src/main/java/jade/Window.java
@@ -3,12 +3,19 @@ package jade;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWWindowFocusCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window{
 
 
    private String title;
    private int width, height;
+
+   private long glfwWindow;
 
    private static Window window = null;
 
@@ -35,11 +42,47 @@ public class Window{
         //error callback
         GLFWErrorCallback.createPrint(System.err).set();
 
-        if()
+        if(!glfwInit()){
+            throw new IllegalStateException("Unable to initialize GLFW.");
+        }
+
+        //Configure GLFW
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE,GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE,GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED,GLFW_TRUE);
+
+        //Create the window
+        glfwWindow = glfwCreateWindow(this.width, this.height,this.title,NULL,NULL);
+        if(glfwWindow == NULL){
+            throw  new IllegalStateException("Failed to create the GLFW window.");
+        }
+
+        //Make the OpenGL context current
+        glfwMakeContextCurrent(glfwWindow);
+        //Enable v-sync (go fast as we can)
+        glfwSwapInterval(1);
+
+        //Make the window visible
+        glfwShowWindow(glfwWindow);
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
     }
 
     public void loop(){
+        while (!glfwWindowShouldClose(glfwWindow)){
+            //Poll event
+            glfwPollEvents();
 
+            glClearColor(1.0f,0.0f,0.0f,1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
     }
 }
 
